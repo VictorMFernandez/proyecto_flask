358439be91e352a182592fc930338e4ae37875bd
Manuel <manuel1508vc@gmail.com>
2025-07-21 18:59:16 -0500

    inciando la conexion a la base de datos

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask, request
+from variables import conexion
 
 app = Flask(__name__)
+print(app.config)
+# el app.config almacenara todas las variables de configuracion de la aplicacion
+# aca inicializamos la conexion a la base de datos
+# al moneto de pasar la aplicacion de flask en esta se encontraras la cadena de coneccion a la BD
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:root:ROOT@localhost:3306/alumnos_db'
 
 
-
-
-
-
-
-
-
-
+conexion.init_app(app)
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
---
+++ variables.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# esta es la coneccion a la base de datos
+# No se recomienda tener mas de una coneccion a la base de datos
+
+conexion = SQLAlchemy()
+
+
+
